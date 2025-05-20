68a136a72187cab7fd26abd12c13c5f9921afb36
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-20 20:40:45 +0800

    added a feature that will be responsible for appending the questions, options and answers to the text file. i made the answer line to make only one line so that i will not have to deal with a blank line on the file reader.

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,1 +1,28 @@
 # Quiz maker with application of oop 
+
+class QuizMaker:
+    def add__questions(self):
+        while True:    
+            subjects = input('What subject would you like?(math, science, history, english)(enter quit to return)')
+            if subjects == "quit":
+                break
+            try: 
+                questions = open(f"{subjects}.txt", "a")
+                questions.write(input('Enter a question: \n'))
+                questions.write(f"a) {input('Enter option a: ')}\n")
+                questions.write(f"a) {input('Enter option b: ')}\n")
+                questions.write(f"a) {input('Enter option c: ')}\n")
+                questions.write(f"a) {input('Enter option d: ')}\n")
+                questions.write(f"The correct answer is: {input('Enter the correct answer: ')}\n")
+            finally:
+                questions.close
+
+quizmaker = QuizMaker()
+quizmaker.add__questions()
+
+
+#ask user what sub
+#open file
+#add question
+#add options
+#close file
