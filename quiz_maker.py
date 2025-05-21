c671115de669584e2d4a7967e84a94b717fb9e04
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-21 23:49:39 +0800

    added a feature that will be responsible for editing the lines or the content of the text file. it will separate each line and then enumerate it which then adds numbers before each line on the file so that the user will be able to identify easily what line they want to replace. and then after that, the edited statement or line will be added. we cannot edit  the line by itself. i mean, we need to overwrite the whole file.

--- quiz_maker.py
+++ quiz_maker.py
@@ -3,26 +3,66 @@
 class QuizMaker:
     def add__questions(self):
         while True:    
+
+            # Chooses the subject the user wants to add questions to
             subjects = input('What subject would you like?(math, science, history, english)(enter quit to return)')
-            if subjects == "quit":
+            if subjects == "quit":      # Returns when 'quit' is used by user
                 break
             try: 
-                questions = open(f"{subjects}.txt", "a")
-                questions.write(input('Enter a question: \n'))
+                questions = open(f"{subjects}.txt", "a")            # Opens file
+                questions.write(input('Enter a question: \n'))      # Adds or appends the questions, options, and answer to the end of the list
                 questions.write(f"a) {input('Enter option a: ')}\n")
                 questions.write(f"a) {input('Enter option b: ')}\n")
                 questions.write(f"a) {input('Enter option c: ')}\n")
                 questions.write(f"a) {input('Enter option d: ')}\n")
                 questions.write(f"The correct answer is: {input('Enter the correct answer: ')}\n")
             finally:
-                questions.close
+                questions.close     # Closes the file
+    
+    def file_editor(self):
+            while True:
+        # Chooses the subject the user wants to add questions to
+                subjects = input('What subject would you like?(math, science, history, english)(enter quit to return)')
+                if subjects == "quit":      # Returns when 'quit' is used by user
+                    break
+                try:
+
+                    # Reads content on the text file
+                    file_content = open(f"{subjects}.txt", "r")
+
+                    # Reads each line in the file and enumerates it
+                    lines = file_content.readlines()
+                    for i, line in enumerate (lines, 1):
+                        print(f"{i} {line.strip()}")
+                    
+                    # Prints the entire text file
+                    print(file_content.read)
+                    
+                    # Allows user to choose what line to edit 
+                    edit_line = int(input('Enter what line you want to edit(Enter 0 to quit): '))
+                    if edit_line == "0":
+                        break
+                    # Asks user what they want to edit on the line
+                    edited_statement = input(f"Enter new statement on line {edit_line}: \n")
+
+                    # Replaces the line
+                    lines[edit_line - 1] = f"{edited_statement}\n"
+
+
+
+                    
+
+
+
+
+
+
 
 quizmaker = QuizMaker()
 quizmaker.add__questions()
 
 
-#ask user what sub
-#open file
-#add question
-#add options
-#close file
+# file editor
+# open file
+# overwrite file to edit
+# close file
