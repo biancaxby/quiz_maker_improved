01b36ed8821ea672d08272f73197bcef7888ecce
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-22 13:51:57 +0800

    added a feature that will read the txt file and the user will be able to start answering the quiz. added counters for the correct answers, questions, and also a line counter to keep track of the limit for displaying the lines. i separated the lines so that we could easily count each line. then i skipped the lines containing the answer key.

--- file_reader_for_quiz_maker.py
+++ file_reader_for_quiz_maker.py
@@ -1,1 +1,26 @@
 # Quiz maker with application of oop
+
+class FileReader:
+    def quiz_file_reader():
+        while True:
+            subjects = input("What subject would you like to do?(math, science, history, english)(Enter quit to exit): ")
+            if subjects == "quit":
+                break
+            try:
+                quiz = open(f"{subjects}.txt", "r")
+                start_quiz = quiz.readlines
+                count_line = 0         # Counts how many lines 
+                correct_answers = 0    # Counts how many correct answers 
+                question_counter = 0 
+               
+                while count_line < len(start_quiz):
+                    for _ in range(5):      # Prints 5 lines of the text file
+                        if count_line >= len(start_quiz):
+                            break      # Exits the loop if the file has no more lines
+
+                        striped_lines = start_quiz[count_line].strip()      # Separates each line
+
+                        # Ignores blank lines and lines with the answer key
+                        if striped_lines == striped_lines.startswith("The correct answer is:"):
+                            count_line += 1
+                            continue
