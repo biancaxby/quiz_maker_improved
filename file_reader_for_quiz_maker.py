67bd35ebb430ee147e1982b6dc6b36c959683c48
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-23 23:33:23 +0800

    added the part where user will enter the correct answer and the program will check whether the answer is correct or wrong. also added a feature that computes the percentage of the questions answered correctly and a messegae that will tell the user how many correct answers they got out of all the questions. also added the username

--- file_reader_for_quiz_maker.py
+++ file_reader_for_quiz_maker.py
@@ -1,14 +1,18 @@
-# Quiz maker with application of oop
+# Quiz maker with application of oop format
 
 class FileReader:
-    def quiz_file_reader():
+    def __init__(self, usernames):
+         self.usernames = usernames
+         
+    def quiz_file_reader(self):
         while True:
+
             subjects = input("What subject would you like to do?(math, science, history, english)(Enter quit to exit): ")
             if subjects == "quit":
                 break
             try:
                 quiz = open(f"{subjects}.txt", "r")
-                start_quiz = quiz.readlines
+                start_quiz = quiz.readlines()
                 count_line = 0         # Counts how many lines 
                 correct_answers = 0    # Counts how many correct answers 
                 question_counter = 0 
@@ -33,3 +37,26 @@ class FileReader:
                         if count_line < len(start_quiz) and start_quiz[count_line].startswith("The correct answer is:"):
                             answer = start_quiz[count_line].replace("The correct answer is: ", "").strip()   # Separates the answer
                             count_line += 1
+
+                            # Asks user for the answer to the question
+                            user_answer = input('What do you think is the answer?: ').lower().strip()
+                            question_counter += 1      # Counts how many questions have been displayed
+
+                            if  user_answer == answer:
+                                    print("Correct!")     # Prints correct if the answer is correct
+                                    correct_answers += 1  # Adds to the number of correct answers
+                            if user_answer != answer:
+                                print("Wrong!")          # Prints wrong is the answer is wrong
+                            if user_answer == "quit":     # Returns to the menu 
+                                    break
+                        
+                percentage = correct_answers / question_counter * 100    # Calculates the percentage of the score 
+                print(f"Congrats {self.usernames}! You have scored {correct_answers} out of {question_counter}!\n Which means you have answered {percentage}% of the questions correctly!")
+            
+            finally:
+                quiz.close()
+
+usernames = input("What is your name?: ")
+file_reader = FileReader(usernames)
+file_reader.quiz_file_reader()
+            
