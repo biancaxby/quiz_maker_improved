bc6efc78875f417b90549428dc481bd2ebcd31a0
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-27 17:56:20 +0800

    imported the class FileReader from another file and then added options for the user to choose from. each number is assigned to do a certain task and when user chose to do that task, then the file will execute the code from another file.

--- main_file_reader_for_quiz_maker.py
+++ main_file_reader_for_quiz_maker.py
@@ -1,1 +1,12 @@
 from file_reader_for_quiz_maker import FileReader
+
+print('1. Start quiz \n2. View top scorers')
+user_input = input('What would you like to do?')
+
+file_reader = FileReader()
+if user_input == '1':
+    file_reader.quiz_file_reader()
+if user_input == '2':
+    file_reader.score_viewer()
+else:
+    print('Invalid input')
