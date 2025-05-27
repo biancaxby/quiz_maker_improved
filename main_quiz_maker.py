612d4af2f8d6831cacf5e12185140cca37c0053b
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-27 16:52:05 +0800

    imported the class QuizMaker from another file so that the main file where the program will be executed is neat and only has a few lines of code. added a feature where user can choose what they want to do and will execute the funtion from the other file.

--- main_quiz_maker.py
+++ main_quiz_maker.py
@@ -1,1 +1,14 @@
 from quiz_maker import QuizMaker
+
+print('1. Make quiz \n2. Edit questions \n3. View questions')
+user_input = input('What would you like to do?(Enter options 1-3)')
+
+quiz = QuizMaker()
+if user_input == '1':
+    quiz.add__questions()
+elif user_input == '2':
+    quiz.file_editor()
+elif user_input == '3':
+    quiz.view_questions()
+else:
+    print('Invalid option')
